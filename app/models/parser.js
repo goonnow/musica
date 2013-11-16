a07d6bd37c52a5ad91ee78fa117e5e5b9a2b2aa0
Pattarawat Chormai <goonnow@rocketmail.com>
2013-11-17 02:11:12 +0700

    Can't show chordtabs vie iframe

--- app/models/parser.js
+++ app/models/parser.js
@@ -8,6 +8,8 @@ exports.parse = function(data) {
         song.title = data.title.replace(/คอร์ด คอร์ดเพลง คอร์ดกีต้าร์ เนื้อเพลง /g,'');
     } else if( data.link.match(/e-chords/) ){
         song.title= data.title;
+    } else if( data.link.match(/chord\.in\.th/) ){
+        song.title= data.title.replace(/คอร์ดเพลง /,'');
     }
 
 
